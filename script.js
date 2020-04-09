ce73515323d45ff7ba69b85849780bb133116e01
William Trout <troutw5593@gmail.com>
2020-04-08 21:47:26 -0500

    adding js, jquerie, momentum, and tags.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// declarations.
+// declarations
+
+var t9 = document.querySelector(".t9").textContent;
+t9 = parseInt(t9[0]);
+var d9 = document.querySelector(".d9");
+
+
+// save descriptions
+
+$(".b9").on("click", function () {
+    var descriptionEntry = $(".t9").val();
+    localStorage.setItem("9:00", descriptionEntry);
+    console.log('b9 was clicked');
+});
+$(".t9").val(localStorage.descriptionEntry("9:00"));
